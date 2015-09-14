93f6cb4d44a29056726ec86a612254c60de3d17d
SaurabhKhera <c0666985@mylambton.ca>
2015-09-14 14:24:22 -0400

    HelloWorld

--- src/cpd3314/asign1/CPD3314Asign1.java
+++ src/cpd3314/asign1/CPD3314Asign1.java
@@ -17,6 +17,29 @@ public class CPD3314Asign1 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World");
+        
+        /**************************
+         * Assignment 1
+         * 
+         * Job Posting : 
+         * http://www.indeed.ca/cmp/Tundra-Technical-Solution/jobs/Java-Developer-a72a26996f875cbe?q=Java
+         * Java Developer (Ottawa) Tundra Technical Solution - Toronto, ON
+         * 
+         * J2EE: Java Enterprise Edition  https://en.wikipedia.org/wiki/Java_Platform,_Enterprise_Edition
+         * SDLC: System Development Life Cycle https://en.wikipedia.org/wiki/Systems_development_life_cycle
+         * AGILE: https://en.wikipedia.org/wiki/Agile_software_development
+         * SQL: Structured Query Language https://en.wikipedia.org/wiki/SQL
+         * ETL: Extract, transform, load https://en.wikipedia.org/wiki/Extract,_transform,_load
+         * SWING: GUI Widget toolkit https://en.wikipedia.org/wiki/Swing_(Java)
+         * RMI: Java Remote Method Invocation https://en.wikipedia.org/wiki/Java_remote_method_invocation
+         * Oracle : Database 
+         * PostgreSQL: object-relational database management system https://en.wikipedia.org/wiki/PostgreSQL
+         * JSON: JavaScript Object Notation https://en.wikipedia.org/wiki/JSON
+         * 
+         * Toronto Salary: 87K
+         * Vancouver Salary: 92K
+         * Waterloo Salaru: 90K
+         */
     }
     
 }
